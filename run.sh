e1ee6f260dc09b890612a4329947f36b01a3e3fc
Tomas Reigl <tomas.reigl@gmail.com>
2023-07-04 11:48:38 +0200

    last is the best

--- run.sh
+++ run.sh
@@ -2,7 +2,27 @@
 
 ../nextflow/launch.sh kuberun nf-core/scrnaseq \
     --input 'samplesheet.csv' \
-    --fasta Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz \
-    --gtf Homo_sapiens.GRCh38.108.gtf.gz \
+    --fasta genome/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz \
+    --gtf genome/Homo_sapiens.GRCh38.109.gtf.gz \
     --outdir 'first' \
     -head-image 'cerit.io/nextflow/nextflow:22.11.1'
+
+../nextflow/launch.sh kuberun nf-core/scrnaseq \
+    --input 'samplesheet.csv' \
+    --fasta genome/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa \
+    --gtf genome/Homo_sapiens.GRCh38.109.gtf \
+    --outdir 'first' \
+    -head-image 'cerit.io/nextflow/nextflow:22.11.1' \
+    -head-memory 4096Mi \
+    -head-cpus 1 \
+    -head-prescript /mnt/nextflow-cfg.sh
+
+../nextflow/launch.sh kuberun nf-core/scrnaseq \
+    --input 'samplesheet.csv' \
+    --fasta genome/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa \
+    --gtf genome/Homo_sapiens.GRCh38.dna_sm.primary_assembly_genes.gtf \
+    --outdir 'first' \
+    -head-image 'cerit.io/nextflow/nextflow:22.11.1' \
+    -head-memory 4096Mi \
+    -head-cpus 1 \
+    -head-prescript /mnt/nextflow-cfg.sh
