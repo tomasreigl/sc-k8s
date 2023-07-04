f95dcd3790bedf2560a075a7620daca645dcde63
Tomas Reigl <tomas.reigl@gmail.com>
2023-07-04 10:28:59 +0200

    Local genome in run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
 ../nextflow/launch.sh kuberun nf-core/scrnaseq --input 'samplesheet.csv' --outdir 'first' --genome GRCh38 -head-image 'cerit.io/nextflow/nextflow:22.11.1'
+
+../nextflow/launch.sh kuberun nf-core/scrnaseq \
+    --input 'samplesheet.csv' \
+    --fasta Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz \
+    --gtf Homo_sapiens.GRCh38.108.gtf.gz \
+    --outdir 'first' \
+    -head-image 'cerit.io/nextflow/nextflow:22.11.1'
