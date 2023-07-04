14254acf13933398484bb2e4f56740ccc02367d1
Tomas Reigl <tomas.reigl@gmail.com>
2023-07-04 16:55:19 +0200

    Plati stale to uplne posledni

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-../nextflow/launch.sh kuberun nf-core/scrnaseq --input 'samplesheet.csv' --outdir 'first' --genome GRCh38 -head-image 'cerit.io/nextflow/nextflow:22.11.1'
-
-../nextflow/launch.sh kuberun nf-core/scrnaseq \
-    --input 'samplesheet.csv' \
-    --fasta genome/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz \
-    --gtf genome/Homo_sapiens.GRCh38.109.gtf.gz \
-    --outdir 'first' \
-    -head-image 'cerit.io/nextflow/nextflow:22.11.1'
+#../nextflow/launch.sh kuberun nf-core/scrnaseq --input 'samplesheet.csv' --outdir 'first' --genome GRCh38 -head-image 'cerit.io/nextflow/nextflow:22.11.1'
+#
+#../nextflow/launch.sh kuberun nf-core/scrnaseq \
+#    --input 'samplesheet.csv' \
+#    --fasta genome/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz \
+#    --gtf genome/Homo_sapiens.GRCh38.109.gtf.gz \
+#    --outdir 'first' \
+#    -head-image 'cerit.io/nextflow/nextflow:22.11.1'
 
 ../nextflow/launch.sh kuberun nf-core/scrnaseq \
     --input 'samplesheet.csv' \
@@ -26,3 +26,44 @@
     -head-memory 4096Mi \
     -head-cpus 1 \
     -head-prescript /mnt/nextflow-cfg.sh
+
+
+../nextflow/launch.sh kuberun nf-core/scrnaseq \
+    --input 'samplesheet.csv' \
+    --fasta genome/GRCh38.fa \
+    --gtf genome/GRCh38.gtf \
+    --outdir 'first' \
+    -head-image 'cerit.io/nextflow/nextflow:22.11.1' \
+    -head-memory 4096Mi \
+    -head-cpus 1 \
+    -head-prescript /mnt/nextflow-cfg.sh
+
+../nextflow/launch.sh kuberun nf-core/scrnaseq \
+    --input 'samplesheet.csv' \
+    --fasta genome/GRCh38.fa \
+    --gtf genome/GRCh38_genes.gtf \
+    --outdir 'first' \
+    -head-image 'cerit.io/nextflow/nextflow:22.11.1' \
+    -head-memory 4096Mi \
+    -head-cpus 1 \
+    -head-prescript /mnt/nextflow-cfg.sh
+
+
+
+../nextflow/launch.sh kuberun nf-core/scrnaseq \
+    --input 'samplesheet.csv' \
+    --fasta genome/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa \
+    --gtf genome/GRCh38.109.gtf \
+    --outdir 'first' \
+    -head-image 'cerit.io/nextflow/nextflow:22.11.1' \
+    -head-memory 4096Mi \
+    -head-cpus 1 \
+    -head-prescript /mnt/nextflow-cfg.sh \
+    -profile test \
+    --max_memory 50.GB \
+    --max_cpus 8
+
+
+
+
+    
